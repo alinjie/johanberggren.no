2d38e4fad956f2a71fbb7ef286419977f122640b
ali.njie <alinjie@Alis-iMac.local>
2020-01-14 20:34:46 +0100

    Image section done for now onClick to view fullscreen will be implemented at a later point

---
+++ src/components/ImageSection/GalleryImages/index.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import "./GalleryImages.css"
+
+const GalleryImages = () => {
+  const data = useStaticQuery(graphql`
+    {
+      galleryImages: allFile(
+        filter: { sourceInstanceName: { eq: "gallery-images" } }
+      ) {
+        nodes {
+          childImageSharp {
+            fixed(height: 250, width: 250) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div className="gallery-images">
+      {data.galleryImages.nodes.map(image => (
+        <Img
+          className="gallery-images__image"
+          fixed={image.childImageSharp.fixed}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default GalleryImages
---
+++ src/components/ImageSection/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Section from "../Section"
+import GalleryImages from "./GalleryImages"
+import SectionTitle from "../SectionTitle"
+const ImageSection = () => {
+  return (
+    <Section id="Galleri">
+      <SectionTitle>Galleri</SectionTitle>
+      <GalleryImages />
+    </Section>
+  )
+}
+
+export default ImageSection

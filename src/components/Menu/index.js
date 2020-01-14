433a1db108cc1ea3999486db0e42913ef2633859
ali.njie <alinjie@Alis-iMac.local>
2020-01-14 18:58:17 +0100

    WIP: Menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { menuItems } from "./menuItems"
+import "./Menu.css"
+
+const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul className="menu__list">
+        {menuItems.map(item => (
+          <li key={item.name} className="menu__list-item">
+            <a href={item.sectionId}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu
---
+++ src/components/Menu/menuItems.js
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    name: "Musikk",
+    sectionId: "#Musikk",
+  },
+  {
+    name: "Bilder",
+    sectionId: "#Bilder",
+  },
+  {
+    name: "Konsert",
+    sectionId: "#Konsert",
+  },
+  {
+    name: "Om",
+    sectionId: "#Om",
+  },
+]

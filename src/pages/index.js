542369460c149138f4bff8791853d078432ac32f
ali.njie <alinjie@Alis-iMac.local>
2020-01-14 19:19:46 +0100

    New section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
 import MusicSection from "../components/MusicSection"
+import ImageSection from "../components/ImageSection"
 
 export default () => (
   <Layout>
     <MusicSection />
+    <ImageSection />
   </Layout>
 )

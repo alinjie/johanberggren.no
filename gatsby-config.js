1e43080432fcf5605e8a4f45260ab368401373fc
ali.njie <alinjie@Alis-iMac.local>
2020-01-14 18:58:47 +0100

    Gatsby scaffold

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+const path = require("path")
+
+module.exports = {
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `music-images`,
+        path: path.join(__dirname, `src`, `assets`, "img", "music"),
+      },
+    },
+  ],
+}

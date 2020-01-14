baa42dafa9edf1b825f7afbc0c0ce20339d0e5b0
ali.njie <alinjie@Alis-iMac.local>
2020-01-14 18:58:33 +0100

    First (and only) page

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Layout from "../components/Layout"
+import MusicSection from "../components/MusicSection"
+
+export default () => (
+  <Layout>
+    <MusicSection />
+  </Layout>
+)

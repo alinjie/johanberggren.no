c5b848171e92473a722bedbec55bd001c4863f10
ali.njie <alinjie@Alis-iMac.local>
2020-01-19 10:35:31 +0100

    WIP: Reworked header a little

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,40 @@
 import React from "react"
 import Logo from "components/Logo"
 import Menu from "components/Menu"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 import "./Header.scss"
 
 const Header = () => {
+  const data = useStaticQuery(graphql`
+    {
+      headerBackground: file(
+        sourceInstanceName: { eq: "general-images" }
+        name: { eq: "header-background" }
+      ) {
+        childImageSharp {
+          fixed {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <header className="header">
-      <div className="header__container">
-        <Logo />
-        <Menu />
-      </div>
-    </header>
+    <BackgroundImage
+      className="header"
+      fixed={data.headerBackground.childImageSharp.fixed}
+      alt="Johan Berggren fotografert i egen sofa."
+      id="top"
+    >
+      <header className="header">
+        <div className="header__container">
+          {/* <Logo /> */}
+          <Menu />
+        </div>
+      </header>
+    </BackgroundImage>
   )
 }
 

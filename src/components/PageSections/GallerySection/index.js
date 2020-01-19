d68b7ee435cabddeb3138fd44980f4cd6262cac7
ali.njie <alinjie@Alis-iMac.local>
2020-01-19 10:36:46 +0100

    Gallery section complete for now. Need to implement onClick on images

---
+++ src/components/PageSections/GallerySection/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Section from "components/Section"
+import GalleryImages from "./GalleryImages"
+
+const ImageSection = () => {
+  return (
+    <Section title="Galleri" id="Galleri">
+      <GalleryImages />
+    </Section>
+  )
+}
+
+export default ImageSection

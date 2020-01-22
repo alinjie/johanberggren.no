115088eea9ef4f01e05cd07af6f144e3c19da13b
ali.njie <alinjie@Alis-iMac.local>
2020-01-22 17:38:47 +0100

    Netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {

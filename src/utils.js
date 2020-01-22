17bf7988042c7183e9536312964c3347269640b0
ali.njie <alinjie@Alis-iMac.local>
2020-01-22 22:51:33 +0100

    Date to desired date format util

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+export const formatDateString = dateString => {
+  const dateObject = new Date(dateString)
+  const day = dateObject.getDate()
+  const month = dateObject.getMonth() + 1
+  const year = dateObject.getFullYear()
+
+  return `${day}.${month <= 9 ? "0" + month : month}.${year}` // Weird if else check because Javascript returns months in a werid way.
+}

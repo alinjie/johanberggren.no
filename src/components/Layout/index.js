aefa3e7d92cee6abacbef492cf97fa76090e5e3b
ali.njie <alinjie@Alis-iMac.local>
2020-01-24 22:41:32 +0100

    Font awesome

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,6 +3,7 @@ import Helmet from "react-helmet"
 import "./Layout.scss"
 import Header from "components/Header"
 import Footer from "components/Footer"
+import "../../assets/font-awesome/css/all.min.css"
 
 const Layout = ({ children }) => {
   return (
@@ -17,10 +18,6 @@ const Layout = ({ children }) => {
           name="description"
           content="Offisiell nettside for Johan Berggren"
         />
-        <script
-          src="https://kit.fontawesome.com/c96ed127ec.js"
-          crossorigin="anonymous"
-        ></script>
       </Helmet>
       <Header />
       {children}

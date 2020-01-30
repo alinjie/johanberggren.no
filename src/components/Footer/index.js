196d01cf94692c0187fc1b3bc3c01bc9c1ae306d
ali.njie <alinjie@Alis-iMac.local>
2020-01-30 22:17:20 +0100

    Closes #7

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,36 @@
 import React from "react"
 import "./Footer.scss"
+import { socialIcons } from "./socialIcons"
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <span className="footer__text">Johan Berggren @ Rootsy Music</span>
+      <div className="footer__content-wrapper">
+        <div className="footer__section">
+          <p className="footer__title">Presse / Booking</p>
+          <p>Vibeke@rootsy.no</p>
+        </div>
+        <div className="footer__section">
+          <p className="footer__title">Distribusjon</p>
+          <p>info@border.se</p>
+        </div>
+        <div className="footer__section">
+          <p className="footer__title">Sosialt</p>
+          <div className="footer__icon-wrapper">
+            {socialIcons.map(icon => (
+              <a
+                key={icon.icon}
+                href={icon.link}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <i className={icon.icon} />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+      <p className="footer__logo">Johan Berggren</p>
     </footer>
   )
 }
---
+++ src/components/Footer/socialIcons.js
@@ -0,0 +1,32 @@
+export const socialIcons = [
+  {
+    icon: "fab fa-facebook",
+    link: "https://www.facebook.com/jbbergren",
+  },
+  {
+    icon: "fab fa-instagram",
+    link: "https://www.instagram.com/jbberggren/",
+  },
+  {
+    icon: "fab fa-youtube",
+    link: "https://www.youtube.com/channel/UCVqzymO-c1r2x3lD4c5lh2g",
+  },
+  {
+    icon: "fab fa-spotify",
+    link:
+      "https://open.spotify.com/album/1Xj0AwMINsMUDTfwIaStnb?si=ukmNcIXuQ2OUyEEV41lACg",
+  },
+  {
+    icon: "fab fa-apple",
+    link: "https://itunes.apple.com/no/artist/johan-berggren/599668517",
+  },
+  {
+    icon: "fab fa-google-play",
+    link:
+      "https://play.google.com/store/music/artist/Johan_Berggren?id=Aweay2zg2m6fpvulycb6xxc7ndm",
+  },
+  {
+    icon: "fab fa-soundcloud",
+    link: "https://soundcloud.com/jbwolsen",
+  },
+]

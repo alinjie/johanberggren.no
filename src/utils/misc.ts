ae382af01e31c7684ff9c752e202b2e10637cf90
ali.njie <alinjie@Alis-iMac.local>
2020-06-13 21:54:27 +0200

    Logical spliot

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,16 +1,5 @@
 import RemarkObject from "interfaces/RemarkObject"
 
-export const formatDateString = (dateString: string) => {
-  const dateObject = new Date(dateString)
-  const day = dateObject.getDate()
-  const month = dateObject.getMonth() + 1
-  const year = dateObject.getFullYear()
-
-  return `${day <= 9 ? "0" + day : day}.${
-    month <= 9 ? "0" + month : month
-  }.${year}` // Weird if else check because Javascript returns months in a werid way.
-}
-
 export const sortEventData = (eventData: RemarkObject[]) => {
   return eventData.sort((a, b) => {
     if (

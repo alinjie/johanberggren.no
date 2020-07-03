35b5369a0977efc2302deca16cfd615811295cdb
ali.njie <alinjie@Alis-iMac.local>
2020-07-03 14:53:14 +0200

    closes #5

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,11 +9,11 @@ export default function Footer() {
       <Container className="footer__content-wrapper">
         <div className="footer__section">
           <p className="footer__title">Presse / Booking</p>
-          <p>Vibeke@rootsy.no</p>
+          <a href="mailto:Vibeke@rootsy.no">Vibeke@rootsy.no</a>
         </div>
         <div className="footer__section">
           <p className="footer__title">Distribusjon</p>
-          <p>info@border.se</p>
+          <a href="mailto:info@border.se">info@border.se</a>
         </div>
         <div className="footer__section">
           <p className="footer__title">Sosialt</p>

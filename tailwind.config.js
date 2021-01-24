3815d1ee35960492d6ad1de9b78c2b335a388d9e
ali.njie <ali.k.njie@gmail.com>
2021-01-24 22:43:57 +0100

    build: purge unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [],
+  purge: [
+    "./src/**/*.tsx"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {

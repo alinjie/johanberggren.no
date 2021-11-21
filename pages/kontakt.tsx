e7837df9c1db7ab4cccb52463e22dc0de53ab0a0
Ali Kristoffer Njie <ali.k.njie@gmail.com>
2021-11-21 16:59:47 +0100

    chore: remove .next folder and resize images

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -39,6 +39,7 @@ export default function Kontakt() {
               <div className="flex gap-2">
                 {SOCIAL_ICONS.map(({ Icon, href }) => (
                   <a
+                    key={href}
                     href={href}
                     className="text-gray-500 hover:text-gray-900 transition-colors"
                     target="_blank"

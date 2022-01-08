2c9f81cb3ae180872ccb840bf1434ef90e5644b2
Ali Kristoffer Njie <ali.k.njie@gmail.com>
2022-01-08 19:47:39 +0100

    chore: add breadcrumbs

--- pages/tekster/[slug]/[number].tsx
+++ pages/tekster/[slug]/[number].tsx
@@ -52,23 +52,63 @@ export default function SongText({
         />
       </div>
       <Container className="mt-10">
-        <Link href={`/tekster/${album.slug}`}>
-          <a className="mb-6 flex items-center text-gray-500 text-sm">
-            <svg
-              className="w-4 h-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span className="ml-1">Tilbake</span>
-          </a>
-        </Link>
+        <div className="mb-8 space-x-1 font-medium flex text-sm text-gray-800">
+          <Link href="/">
+            <a>
+              <svg
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+              </svg>
+            </a>
+          </Link>
+          <svg
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <Link href="/tekster">
+            <a className="hover:underline">Tekster</a>
+          </Link>
+          <svg
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <Link href={`/tekster/${album.slug}`}>
+            <a className="hover:underline">{album.name}</a>
+          </Link>
+          <svg
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span className="text-gray-500 font-normal">{song.name}</span>
+        </div>
         <div dangerouslySetInnerHTML={{ __html: html }} className="prose" />
         <div className="grid grid-cols-2 mt-8 border-t pt-4">
           <div>
--- pages/tekster/[slug]/index.tsx
+++ pages/tekster/[slug]/index.tsx
@@ -47,27 +47,72 @@ export default function AlbumLyrics({
           className="filter blur-xl scale-105 transform"
         />
       </div>
-      <Container className="mt-10 divide-y">
-        {tracks
-          .sort((a, b) => a.number - b.number)
-          .map((track, index) => (
-            <Link href={`/tekster/${album.slug}/${track.number}`}>
-              <a
-                className={cx(
-                  "block py-4",
-                  index === 0 && "pt-0",
-                  index === tracks.length - 1 && "pb-0"
-                )}
+      <Container className="mt-10 ">
+        <div className="mb-10 space-x-1 font-medium flex text-sm text-gray-800">
+          <Link href="/">
+            <a>
+              <svg
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <h2 className="font-medium mb-1">
-                  {track.number}. {track.name}
-                </h2>
-                <span className="text-gray-600  text-sm block">
-                  {album.name}
-                </span>
-              </a>
-            </Link>
-          ))}
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+              </svg>
+            </a>
+          </Link>
+          <svg
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <Link href="/tekster">
+            <a className="hover:underline">Tekster</a>
+          </Link>
+          <svg
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <span className="text-gray-500">{album.name}</span>
+        </div>
+        <div className="divide-y">
+          {tracks
+            .sort((a, b) => a.number - b.number)
+            .map((track, index) => (
+              <Link href={`/tekster/${album.slug}/${track.number}`}>
+                <a
+                  className={cx(
+                    "block py-4",
+                    index === 0 && "pt-0",
+                    index === tracks.length - 1 && "pb-0"
+                  )}
+                >
+                  <h2 className="font-medium mb-1">
+                    {track.number}. {track.name}
+                  </h2>
+                  <span className="text-gray-600  text-sm block">
+                    {album.name}
+                  </span>
+                </a>
+              </Link>
+            ))}
+        </div>
       </Container>
     </div>
   )

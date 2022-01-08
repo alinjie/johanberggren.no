3a4cf48a660e9c0587bdae9e9360ee969b81695a
Ali Kristoffer Njie <ali.k.njie@gmail.com>
2022-01-08 19:52:03 +0100

    style: remove footer border

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,9 @@
-import dayjs from "dayjs";
+import dayjs from "dayjs"
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-gray-200 py-6 flex justify-center text-sm text-gray-500 mt-10">
+    <footer className="w-full py-6 flex justify-center text-sm text-gray-500 mt-10">
       © Copyright Johan Berggren {dayjs().format("YYYY")}
     </footer>
-  );
+  )
 }
